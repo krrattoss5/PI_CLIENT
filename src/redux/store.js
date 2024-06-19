0c6ead3edcfac684380a21ab810fd11f876be037
krrattoss5 <krrattoss5@gmail.com>
2024-06-19 13:44:29 -0500

    dependencies updateds

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,24 @@
-import {createStore,applyMiddleware}from 'redux';
-import rootReducer from './reducer.js';
-import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
-export default store;
+export const store = configureStore({
+  reducer:
+})
+
+
+
+
+
+
+
+// import {createStore,applyMiddleware}from 'redux';
+// import rootReducer from './reducer.js';
+// import thunk from 'redux-thunk';
+// import {composeWithDevTools} from 'redux-devtools-extension';
+
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+// export default store;
